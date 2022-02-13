58c9d29c03f8c64cf40d99629abadcba56f6b119
Bababooey <DG246670@falmouth.ac.uk>
2022-02-13 17:18:52 +0000

    Client Setup Added

--- src/main/java/com/stoneshapers/albrgw/setup/ModSetup.java
+++ src/main/java/com/stoneshapers/albrgw/setup/ModSetup.java
@@ -1,9 +1,27 @@
 package com.stoneshapers.albrgw.setup;
 
-import net.minecraftforge.eventbus.api.Event;
+
+import com.stoneshapers.albrgw.Alabaster;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
+
+
 public class ModSetup {
-    public static void init(FMLCommonSetupEvent event) {
+    public static final String TAB_NAME = "Alabaster";
+
+    public static final CreativeModeTab ITEM_GROUP = new CreativeModeTab() {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(Registration.MYSTERIOUS_INGOT.get());
+        }
+    };
+    public static void init(final FMLCommonSetupEvent event) {
+
     }
 }
+
+
